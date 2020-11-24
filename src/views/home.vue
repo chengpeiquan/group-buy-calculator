2c6111e24b213bf84bdbf1a2c926ea8fd0c0630f
chengpeiquan <chengpeiquan@chengpeiquan.com>
2020-11-24 08:35:14 +0800

    backup.

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="data">
     
     <!-- 原价 -->
@@ -7,30 +6,10 @@
       <template #extra>
         <a class="add" @click="add"><PlusOutlined /> 添加一项</a>
       </template>
-=======
-
-  <a-list :grid="{ gutter: 16, column: 4 }" :data-source="cardData">
-    <template #renderItem="{ item, index }">
-      <a-list-item>
-        <a-card :title="item">
-          Card content
-        </a-card>
-      </a-list-item>
-    </template>
-  </a-list>
-
-
-  <div class="data">
-    
-    <!-- 原价 -->
-    <a-card title="填写原价信息">
-      <template #extra><a href="#">添加一个</a></template>
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
 
       <section class="table">
         <a-divider orientation="left">购买信息</a-divider>
 
-<<<<<<< HEAD
         <ul class="list">
           <li class="item"
             v-for="(item, index) in dataList"
@@ -92,28 +71,14 @@
             </label>
 
             <DeleteOutlined @click="remove(index)" />
-=======
-        <ul>
-          <li>
-            <DeleteOutlined />
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
           </li>
         </ul>
       </section>
       
-<<<<<<< HEAD
       <a-divider />
 
       <section class="btn">
         <a-button type="primary" @click="getResult">点击计算结果</a-button>
-=======
-      <a-divider orientation="left">公摊费用</a-divider>
-
-      <a-divider />
-
-      <section class="btn">
-        <a-button type="dashed">点击计算结果</a-button>
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
       </section>
     </a-card>
     <!-- 原价 -->
@@ -121,26 +86,18 @@
     <a-divider class="placeholder" type="vertical" />
 
     <!-- 实价 -->
-<<<<<<< HEAD
     <a-card class="result-area" title="计算后的结果">
-=======
-    <a-card title="计算后的结果">
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
       <template #extra><a href="#">more</a></template>
       <p>card content</p>
       <p>card content</p>
       <p>card content</p>
     </a-card>
     <!-- 实价 -->
-<<<<<<< HEAD
 
-=======
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
   </div>
 </template>
 
 <script lang="ts">
-<<<<<<< HEAD
 import { defineComponent, reactive, ref } from 'vue'
 import { PlusOutlined, DeleteOutlined } from '@ant-design/icons-vue'
 
@@ -152,18 +109,10 @@ interface Data {
 export default defineComponent({
   components: {
     PlusOutlined,
-=======
-import { defineComponent } from 'vue'
-import { DeleteOutlined } from '@ant-design/icons-vue'
-
-export default defineComponent({
-  components: {
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
     DeleteOutlined
   },
   setup () {
     const cardData = [ '填写原价信息', '计算后的结果' ];
-<<<<<<< HEAD
     const dataList: Data[] = reactive([
       {
         name: '',
@@ -211,19 +160,12 @@ export default defineComponent({
       add,
       remove,
       getResult
-=======
-    
-
-    return {
-      cardData
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
     }
   }
 })
 </script>
 
 <style lang="stylus" scoped>
-<<<<<<< HEAD
 @import "~@styl/config"
 
 .data
@@ -234,14 +176,6 @@ export default defineComponent({
   margin 20px auto
   .ant-list
     width 100%
-=======
-.data
-  display flex
-  justify-content space-between
-  align-items center
-  max-width 1200px
-  margin 20px auto
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
   .ant-card
     display flex
     flex-direction column
@@ -249,7 +183,6 @@ export default defineComponent({
     .ant-divider
       >>> .ant-divider-inner-text
         font-size 14px
-<<<<<<< HEAD
   // 左侧 - 输入区域
   .input-area
     .table
@@ -276,19 +209,12 @@ export default defineComponent({
             display flex
             flex-shrink 0
             cursor pointer
-=======
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
     .btn
       display flex
       justify-content flex-end
       align-items center
-<<<<<<< HEAD
   // 中间占位符
   .placeholder
     width 10px
-=======
-  .placeholder
-    width 40px
->>>>>>> f3fef24d55154f61e5ab7adf0ad8a60832a80104
     opacity 0
 </style>
