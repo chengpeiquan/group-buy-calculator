2646b706cf1dec04506c995e6859a91d2d77859a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2020-11-25 13:49:15 +0800

    update.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref, watchEffect } from 'vue'
 import Home from '@views/home.vue'
 
 export default defineComponent({
@@ -12,9 +12,30 @@ export default defineComponent({
     Home
   },
   setup () {
-    
+    /** 
+     * 根据访问设备添加样式名
+     */
+    const checkDevice = (): any => {
+			const CLIENT_WIDTH: number = document.documentElement.clientWidth;
+			const CLIENT_HEIGHT: number = document.documentElement.clientHeight;
+      if ( !CLIENT_WIDTH ) { 
+        return false;
+      }
 
-    return {}
+      // 判断是否是手机设备
+      const IS_MOBILE: boolean = /iPhone|phone|android|iPod|pad|iPad/i.test(navigator.userAgent.toLowerCase());
+
+      // 添加样式名
+      (document as any).querySelector('body').className = IS_MOBILE ? 'mode-mobile' : 'mode-desktop';
+    }
+    
+    onMounted( () => {
+      checkDevice();
+    })
+
+    return {
+      checkDevice
+    }
   }
 })
 </script>
