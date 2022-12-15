608aae703be21f27ddd4a8f265566cc3011a9b6a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2022-12-16 01:21:51 +0800

    chore: backup

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,42 @@ import react from '@vitejs/plugin-react'
 import banner from 'vite-plugin-banner'
 import pkg from './package.json'
 
+const manualChunks = {}
+for (const key in pkg.dependencies) {
+  if (Object.prototype.hasOwnProperty.call(pkg.dependencies, key)) {
+    manualChunks[key] = key
+  }
+}
+console.log(manualChunks)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/group-buy-calculator/',
   server: {
     port: 1949,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // 其中一种方式
+        manualChunks: {
+          antd: ['antd'],
+          react: ['react'],
+          'react-dom': ['react-dom'],
+        },
+
+        // 另外一种方式
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return id
+        //       .toString()
+        //       .split('node_modules/')[1]
+        //       .split('/')[0]
+        //       .toString()
+        //   }
+        // },
+      },
+    },
   },
   css: {
     modules: {
